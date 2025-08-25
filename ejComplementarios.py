822cedb9ce55810bf5a621a252adacb628602a00
TekkNo0 <maximonardezz@gmail.com>
2025-08-25 15:34:30 -0300

    añado los ejerciciós complementarios

---
+++ ejComplementarios.py
@@ -0,0 +1,69 @@
+#----------------Ejercició 1------------------
+#numero1 = 1
+#print(numero1)
+
+#----------------Ejercició 2------------------
+#numero2 = 1.6
+#print(numero2)
+
+#----------------Ejercició 3------------------
+#suma = numero2 + numero1
+#print(suma)
+
+#----------------Ejercició 4------------------
+#resta = numero1 - numero2
+#multiplicacion = numero2 * numero1
+#division = numero1 / numero2
+#print(resta,multiplicacion,division)
+
+#----------------Ejercició 5------------------
+#nombre = "Maximo"
+#print(nombre)
+
+#----------------Ejercició 6------------------
+#precio = 49.99
+#print(precio)
+
+#----------------Ejercició 7------------------
+#descuento = 0.20
+#print(descuento)
+
+#----------------Ejercició 8------------------
+#precio_final = precio - (precio * 0.20)
+#print(precio_final)
+
+#----------------Ejercició 9------------------
+#cadena = "Mi perro es amarillo"
+#print(cadena)
+
+#----------------Ejercició 10------------------
+#longitud = len(cadena)
+#print(longitud)
+
+#----------------Ejercició 11------------------
+#precio = int(23.05)
+#print(precio)
+
+#----------------Ejercició 12------------------
+#nombre = "Maximo"
+#apellido = "Monardez"
+#nombre_completo = nombre + " " + apellido
+#print(nombre_completo)
+
+#----------------Ejercició 13------------------
+#edad = (18 + 5) - 10
+#print(edad)
+
+#----------------Ejercició 14------------------
+#altura = (1.76 * 4) / 3
+#print(altura)
+
+#----------------Ejercició 15------------------
+#nombre = "MAXIMO MONARDEZ"
+#nombre = nombre.lower()
+#print(nombre)
+
+#----------------Ejercició 16------------------
+#nombre = "MAXIMO MONARDEZ"
+#nombre = nombre.capitalize()
+#print(nombre)
